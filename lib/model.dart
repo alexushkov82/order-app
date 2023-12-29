516b22692e9403d511315549af059008f0647de6
Alex Ushkov <alexushkov82@gmail.com>
2023-12-29 12:23:27 +0300

    Create models.

---
+++ lib/model.dart
@@ -0,0 +1,46 @@
+class Category {
+  int id;
+  String name;
+
+  Category({required this.id, required this.name});
+
+  Map<String, dynamic> toMap() {
+    return {'id': id, 'name': name};
+  }
+}
+
+class Product {
+  int id;
+  int categoryId;
+  String name;
+
+  Product({required this.id, required this.categoryId, required this.name});
+
+  Map<String, dynamic> toMap() {
+    return {'id': id, 'categoryId': categoryId, 'name': name};
+  }
+}
+
+class Order {
+  int id;
+  int tableNumber;
+
+  Order({required this.id, required this.tableNumber});
+
+  Map<String, dynamic> toMap() {
+    return {'id': id, 'tableNumber': tableNumber};
+  }
+}
+
+class OrderProduct {
+  int id;
+  int orderId;
+  int productId;
+  int quantity;
+
+  OrderProduct({required this.id, required this.orderId, required this.productId, required this.quantity});
+
+  Map<String, dynamic> toMap() {
+    return {'id': id, 'orderId': orderId, 'productId': productId, 'quantity': quantity};
+  }
+}
